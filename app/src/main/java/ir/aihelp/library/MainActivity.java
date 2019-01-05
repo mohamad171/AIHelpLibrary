6642a67d8b3fb8c6909043ebcfe43b0f1adb39d6
mohamad <mohamadmohamadi249@gmail.com>
2019-01-05 14:00:27 +0330

    Copy From Chatsystem to this Project

--- app/src/main/java/ir/aihelp/library/MainActivity.java
+++ app/src/main/java/ir/aihelp/library/MainActivity.java
@@ -1,13 +1,33 @@
 package ir.aihelp.library;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ServerInterface serverInterface;
+
+
+    @Override
+    public void onBackPressed() {
+        getSupportFragmentManager().popBackStack();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.chat_frame,new MainFragment());
+        fragmentTransaction.commit();
     }
+
 }
---
+++ app/src/main/java/ir/aihelp/library/Profile.java
@@ -0,0 +1,22 @@
+package ir.aihelp.library;
+
+/**
+ * Created by mohamad on 08/24/2017.
+ */
+
+public class Profile {
+    public static String userid;
+    public static String profileimage;
+    public static String username;
+    public static String fname;
+    public static String lname;
+    public static String coin;
+    public static String score;
+    public static String email;
+    public static String gender;
+    public static String age;
+    public static String fteam;
+    public static String rating;
+    public static String phone;
+
+}
